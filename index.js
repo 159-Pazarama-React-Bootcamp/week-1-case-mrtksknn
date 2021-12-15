d7f97884e2726d08b39511296a8fdae589e61496
mert keskin <36448206+mrtksknn@users.noreply.github.com>
2021-12-15 17:55:52 +0300

    added: comments to make more understandable

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var str = '6666666666666662';
 
-function Length(str) {
+function Length(str) { // control the card number length is valid
   if (str.length == 16) {
     return true
   }
   return false
 }
 
-function Bigger(str) {
+function Bigger(str) { // control the card number total sum is bigger than 16 
   var sum = 0
   for (var i = 0; i < str.length; i++) {
-    sum += parseInt(str[i])
+    sum += parseInt(str[i]) // I send credit car number as a string so I use parseInt to convert from string to number
   }
   if(sum <= 16) {
     return false
@@ -18,25 +18,25 @@ function Bigger(str) {
   return true
 }
 
-function Same(str) {
+function Same(str) { // control all the numbers is same or not
   var i = 0;
     for (let i = 1; i < str.length; i++)
-      if (str[i] != str[0])
+      if (str[i] != str[0]) // take the first index of number and compare with others
         return true;
  
     return false;
     
 }
 
-function LastChar(str) {
-  var last = str.slice(-1);
+function LastChar(str) { // to control the last char is odd or even
+  var last = str.slice(-1); // getting the last index of number
   if(last % 2 == 0) {
     return true
   }
   return false
 }
 
-function isNumeric(str) {
+function isNumeric(str) { // to control the card numebr has a non numeric char
     console.log( !isNaN(str - parseFloat(str)))
 }
 
